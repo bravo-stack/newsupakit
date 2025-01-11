98bb098c14a15c75f7291f679237b819d67096e7
bravo-stack <chukwuebukaonuh23@gmail.com>
2025-01-11 17:54:48 +0100

    chore: implement delete and update functionality for todos

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { Todo } from '@/types/custom';
 import { createClient } from '@/utils/supabase/server';
 import { revalidatePath } from 'next/cache';
 
@@ -26,6 +27,50 @@ export async function addTodo(formData: FormData) {
 
 	if (error) {
 		throw new Error('Error adding task');
+	}
+
+	revalidatePath('/todos');
+}
+
+export async function deleteTodo(id: number) {
+	const supabase = await createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	if (!user) {
+		throw new Error('User is not logged in');
+	}
+
+	const { error } = await supabase
+		.from('todos')
+		.delete()
+		.match({ id, user_id: user.id });
+
+	if (error) {
+		throw new Error('Error deleting task');
+	}
+
+	revalidatePath('/todos');
+}
+
+export async function updateTodo(todo: Todo) {
+	const supabase = await createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	if (!user) {
+		throw new Error('User is not logged in');
+	}
+
+	const { error } = await supabase
+		.from('todos')
+		.update(todo)
+		.match({ user_id: user.id, id: todo.id });
+
+	if (error) {
+		throw new Error('Error updating task');
 	}
 
 	revalidatePath('/todos');
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { deleteTodo, updateTodo } from '@/app/todos/actions';
 import { cn } from '@/lib/utils';
 import { Todo } from '@/types/custom';
 import { Trash2 } from 'lucide-react';
+import { useFormStatus } from 'react-dom';
 import { Button } from './ui/button';
 import { Card, CardContent } from './ui/card';
 import { Checkbox } from './ui/checkbox';
@@ -18,15 +22,27 @@ export function TodoItem({ todo }: Props) {
 }
 
 function TodoCard({ todo }: Props) {
+	const { pending } = useFormStatus();
 	return (
-		<Card className={cn('w-full')}>
+		<Card className={cn('w-full', pending && 'opacity-50')}>
 			<CardContent className='flex items-start gap-3 p-3'>
 				<span className='size-10 flex items-center justify-center'>
-					<Checkbox />
+					<Checkbox
+						disabled={pending}
+						checked={Boolean(todo.is_complete)}
+						onCheckedChange={async (value) => {
+							if (value === 'indeterminate') return;
+							await updateTodo({ ...todo, is_complete: value as boolean });
+						}}
+					/>
 				</span>
 
 				<p className={cn('flex-1 pt-2 min-w-0 break-words')}>{todo.task}</p>
 				<Button
+					disabled={pending}
+					formAction={async () => {
+						await deleteTodo(todo.id);
+					}}
 					variant='ghost'
 					size='icon'>
 					<Trash2 className='size-5' />
